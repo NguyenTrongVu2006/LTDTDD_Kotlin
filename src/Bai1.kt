caa718e0641dcd6916b4916a47bf6c64fbfe1265
NguyenTrongVu2006 <trongvu670@gmail.com>
2026-09-06 16:08:45 +0700

    update BT

--- src/Bai1.kt
+++ src/Bai1.kt
@@ -1,27 +1,21 @@
 fun main() {
-    print("Nhập mã sinh viên: ")
-    val studentId = readln().trim()
-    print("Nhập họ và tên: ")
-    val fullName = readln().trim()
-    print("Nhập điểm Math: ")
-    val math = readln().toDouble()
-    print("Nhập điểm Programming: ")
-    val programming = readln().toDouble()
-    print("Nhập điểm Database: ")
-    val database = readln().toDouble()
+    val maSV = "2415053122150"
+    val fullName = "Nguyen Trong Vu"
+    val math = 8.5
+    val programming = 9.0
+    val database = 7.5
 
     val tongDiem = math + programming + database
     val gpa = tongDiem / 3.0
     val diemCaoNhat = maxOf(math, programming, database)
     val pass = gpa >= 5.0
 
-    println("\n================ KẾT QUẢ HỌC TẬP ================")
-    println("Mã sinh viên      : $studentId")
-    println("Họ và tên         : $fullName")
-    println("-------------------------------------------------")
-    println("Điểm chi tiết     : Math: $math | Programming: $programming | Database: $database")
-    println("- Tổng điểm       : ${String.format("%.2f", tongDiem)}")
-    println("- Điểm trung bình : ${String.format("%.2f", gpa)}")
-    println("- Điểm cao nhất   : $diemCaoNhat")
-    println("- Kết quả đánh giá: ${if (pass) "ĐẠT" else "KHÔNG ĐẠT"}")
+    println("================ KẾT QUẢ HỌC TẬP ================")
+    println("Mã sinh viên     : $maSV")
+    println("Họ và tên        : $fullName")
+    println("Điểm chi tiết    : Math: $math | Programming: $programming | Database: $database")
+    println(" Tổng điểm       : ${String.format("%.2f", tongDiem)}")
+    println(" Điểm trung bình : ${String.format("%.2f", gpa)}")
+    println(" Điểm cao nhất   : $diemCaoNhat")
+    println("Kết quả đánh giá : ${if (pass) "ĐẠT" else "KHÔNG ĐẠT"}")
 }
